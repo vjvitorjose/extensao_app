2c8cfa1882b51ea0446a1957a017e3ad09ee51bf
Ian Quadrio Raposo Branco Nunes <ianqrbn06@gmail.com>
2026-05-19 16:57:30 -0300

    implements profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'theme/app_colors.dart';
-import 'screens/main_screen.dart';
-import 'screens/login_screen.dart'; 
+import 'screens/auth_gate.dart'; // <-- A importação mágica do nosso novo Guardião!
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,32 +30,10 @@ class SafeHerApp extends StatelessWidget {
           seedColor: AppColors.primary,
           secondary: AppColors.sosRed,
         ),
-        fontFamily: 'Roboto', 
+        fontFamily: 'Roboto',
       ),
+      // O AuthGate importado lá de cima assume o controle da tela inicial
       home: const AuthGate(),
-    );
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<AuthState>(
-      stream: Supabase.instance.client.auth.onAuthStateChange,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(body: Center(child: CircularProgressIndicator()));
-        }
-
-        final session = snapshot.data?.session;
-        if (session != null) {
-          return const MainScreen();
-        } else {
-          return const LoginScreen();
-        }
-      },
     );
   }
 }
